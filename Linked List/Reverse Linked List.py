19657383458fba737adf041e0a24146bf320e041
sohbanm <99363017+sohbanm@users.noreply.github.com>
2022-10-04 17:09:40 -0400

    Create Reverse Linked List.py

---
+++ Linked List/Reverse Linked List.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            nxt = curr.next #so that we don't lose the list
+            curr.next = prev #changing pointer
+            prev = curr 
+            curr = nxt
+        
+        return prev
