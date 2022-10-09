9aabf854e28fca3a03227025ab511f3d855d4fc8
sohbanm <99363017+sohbanm@users.noreply.github.com>
2022-10-08 21:38:00 -0400

    Update Merge Intervals.py

--- Merge Intervals.py
+++ Merge Intervals.py
@@ -1,24 +1,19 @@
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if len(intervals):
-            return intervals
-        outArr = []
+    def merge(self, intervals):
+        #O(nlogn)
+        intervals.sort(key = lambda i: i[0])
+        output = [intervals[0]]
 
-        l, r = 0, 1
-        while r < len(intervals):
-            left = intervals[l]
-            right = intervals[r]
-            if left[0] <= right[0]:
-                if left[1] <= right[1]:
-                    outArr.append([left[0],right[1]])
-                else:
-                    outArr.append(left)
-                    outArr.append(right)
-            # else:
-            #     if left[1] <= right[1]:
-            #         outArr.append([right])
-            #     else:
-            #         outArr.append([left[0],right[1]])
+        for start, end in intervals[1:]:
+            lastEnd = output[-1][1]
 
-        return outArr
-                    
+            if start <= lastEnd:
+                output[-1][1] = max(lastEnd, end)
+            else:
+                output.append([start, end])
+
+        return output
+
+
+sol = Solution()
+sol.merge(intervals = [[1,3],[2,6],[8,10],[15,18]])
