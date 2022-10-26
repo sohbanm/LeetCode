b96b0a7ff2689254e3dd7e03db21187d3ae2c3e4
sohbanm <99363017+sohbanm@users.noreply.github.com>
2022-10-26 11:46:24 -0400

    progress
    integer to roman not done

---
+++ Strings/Integer to Roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman ={1:'I',4:'IV',5:'V',9:'IX',10:'X',40:'XL',50:'L',90:'XC',100:'C',400:'CD',500:'D',900:'CM',1000:'M'}
+        romanIndex = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+        out = ''
+        index = 12
+        while num:
+            if romanIndex[index]<num:
+                num-=romanIndex[index]
+            else:
+                index-=1
+      
+        return out
+        
