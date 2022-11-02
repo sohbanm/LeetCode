dcac9a13c5931c4c4f9bd97529c841cd78b30e52
sohbanm <99363017+sohbanm@users.noreply.github.com>
2022-11-02 15:30:19 -0400

    stuff

---
+++ Tree/invert bst.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+        
+        prev = root.right
+        root.right = root.left
+        self.invertTree(root.right)
+        root.left = prev
+        self.invertTree(root.left)
+        
+        return root
