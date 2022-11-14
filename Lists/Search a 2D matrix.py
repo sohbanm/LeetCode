1805a6fc7c9080b75d1c19da8a47707f4365c262
sohbanm <99363017+sohbanm@users.noreply.github.com>
2022-11-13 19:56:46 -0500

    Update Search a 2D matrix.py

--- Lists/Search a 2D matrix.py
+++ Lists/Search a 2D matrix.py
@@ -1,24 +1,24 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        l,r = 0, len(matrix)-1
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         l,r = 0, len(matrix)-1
         
-        while l<=r:
-            mid = (l+r) // 2
-            if matrix[mid][0]<target:
-                left, right = 0, len(matrix[mid])-1
-                while left<=right:
-                    middle = (left+right)//2
-                    if matrix[mid][middle]==target:
-                        return True
-                    if matrix[mid][middle]<target:
-                        left = middle+1
-                    else:
-                        right = middle-1 
-                l = mid + 1
-            elif matrix[mid][0]>target:
-                r = mid - 1
-            else:
-                return True
+#         while l<=r:
+#             mid = (l+r) // 2
+#             if matrix[mid][0]<target:
+#                 left, right = 0, len(matrix[mid])-1
+#                 while left<=right:
+#                     middle = (left+right)//2
+#                     if matrix[mid][middle]==target:
+#                         return True
+#                     if matrix[mid][middle]<target:
+#                         left = middle+1
+#                     else:
+#                         right = middle-1 
+#                 l = mid + 1
+#             elif matrix[mid][0]>target:
+#                 r = mid - 1
+#             else:
+#                 return True
             
-        return False
-                
+#         return False
+print(0//1)
