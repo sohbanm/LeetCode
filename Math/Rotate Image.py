991778f82c3ec86835b9c9052647559a26694f07
Sohban Elahi Malik <99363017+sohbanm@users.noreply.github.com>
2023-03-27 18:03:43 -0400

    Create Rotate Image.py

---
+++ Math/Rotate Image.py
@@ -0,0 +1,28 @@
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
+
+                #save topleft
+                topLeft = matrix[top][l + i]
+
+                #move the bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                #move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+
+                #move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                #move top left into top right
+                matrix[top + i][r] = topLeft
+
+            r -= 1
+            l += 1
