951dd764cb7c3c017728a627537e445881994c2d
Sohban Elahi Malik <99363017+sohbanm@users.noreply.github.com>
2023-04-25 18:06:39 -0400

    stuff

--- Lists/Letter Combinations of a Phone Number.py
+++ Lists/Letter Combinations of a Phone Number.py
@@ -1,4 +1,4 @@
- class Solution:
+class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         hash = {"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
         res = []
