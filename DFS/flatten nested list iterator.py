7703a67ce5adbd5d53f493c498d43daa4e87f1fe
Sohban Elahi Malik <99363017+sohbanm@users.noreply.github.com>
2023-11-03 16:49:46 -0400

    new shtuff

---
+++ DFS/flatten nested list iterator.py
@@ -0,0 +1,50 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.lis = []
+        
+        for num in nestedList:
+            self.dfs(num)
+
+        self.lis.reverse()
+    
+    def next(self) -> int:
+        return self.lis.pop() 
+    
+    def hasNext(self) -> bool:
+        return len(self.lis) > 0
+    
+    def dfs(self, x):
+        if x.isInteger():
+            self.lis.append(x.getInteger())
+            return
+
+        for num in x.getList():
+            self.dfs(num)
+        
+         
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
