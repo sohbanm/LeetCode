d848bdc854eb3491340865133f031f37cd6e50ad
Sohban Elahi Malik <99363017+sohbanm@users.noreply.github.com>
2023-11-03 00:12:51 -0400

    stuff

--- DS/sorting.py
+++ DS/sorting.py
@@ -19,3 +19,42 @@ def quick_sort(sequence):
     return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
 
 print(quick_sort([8,3,7,2,9,4,5,11,1]))
+
+
+#Merge Sort
+#Worse case, avg case, best case are all O(nlogn)
+"""This is the standard implementation for merge sort which I will be 
+applying to both prefa and prefa and counting their inverts"""
+def merge_sort(Arr: list) -> list:
+    if len(Arr) > 1: 
+        m = len(Arr) // 2
+        leftArr, rightArr = Arr[:m], Arr[m:]
+        
+        #recursion
+        merge_sort(leftArr)
+        merge_sort(rightArr)
+
+        #merge left and right
+        i, j = 0, 0
+        k = 0
+        while i < len(leftArr) and j < len(rightArr):
+            if leftArr[i] < rightArr[j]:
+                Arr[k] = leftArr[i]
+                i += 1
+            else:
+                Arr[k] = rightArr[j]
+                #each time there is an invert I add the pair of values as a tuple
+                j += 1
+            k += 1
+        
+        while i < len(leftArr):
+            Arr[k] = leftArr[i]
+            i += 1
+            k += 1
+        
+        while j < len(rightArr):
+            Arr[k] = rightArr[j]
+            j += 1
+            k += 1
+
+        return Arr
